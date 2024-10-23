356e946d2327c0dfca6c3a7a0fc424dcf0da19b2
xanthos84 <github@nnn.to>
2024-10-23 20:11:05 +0200

    Update midi_remove_silence.py
    fixed a bug where notes are shifted and removing aftertouch made notes disappear

--- midi_remove_silence.py
+++ midi_remove_silence.py
@@ -1,47 +1,71 @@
 import os
 from mido import MidiFile, MidiTrack
+import math
+
+def subtract_until_non_negative(number, decrement):
+    while number - decrement >= 0:
+        number -= decrement
+    return number
+
 
 def remove_silence_from_midi(input_path, output_path):
     """
-    Entfernt die Stille am Anfang einer MIDI-Datei und setzt die Zeit der ersten Nachricht auf 0, falls sie größer als 15000 ist.
+    Entfernt die Stille am Anfang einer MIDI-Datei
     
     :param input_path: Pfad zur Eingabedatei.
     :param output_path: Pfad zur Ausgabedatei.
     """
     mid = MidiFile(input_path)
-    new_mid = MidiFile()
+    new_mid = MidiFile(ticks_per_beat=mid.ticks_per_beat)
 
     for i, track in enumerate(mid.tracks):
         new_track = MidiTrack()
-        notes_started = False
+        tempo = 0
+        bpm = 0
+        bpm_original = 0
+        first_note_time = None
         
         # Variable zum Überprüfen der ersten 'note_on' Nachricht
         first_note_checked = False
-        # Variable zum Überprüfen der ersten 'aftertouch' Nachricht
-        first_aftertouch_checked = False
+
         
         for msg in track:
-            print(msg)
+            print(f"Noteinformation: Type: {msg.type} " + str(msg))
+            
             if msg.type == 'channel_prefix':
                 print(f"Found 'channel_prefix' MetaMessage in Track {i} with channel {msg.channel} and time {msg.time}. Setting time to 0.")
-                # time auf 0 setzen
-                msg.time = 0
+                continue
+            
+            # ACHTUNG - Aftertouch NICHT entfernen, midi file ändert sich dadurch manchmal das timing!
+            # Deswegen als Workaround die values auf 0 gesetzt.. somit stören sie nicht mehr
             if msg.type == 'aftertouch':
-                if not first_aftertouch_checked:
-                    msg.time = 0
-                    print(f"Aftertouch found, cleaning...")
-                    first_aftertouch_checked = True
+                    msg.value = 0
+                    print(f"Aftertouch found, set value to 0...")
+
+            if msg.type == 'set_tempo':
+                    tempo = msg.tempo
+                    print(f"Tempo: {tempo}")
+                    bpm_original = 60000000.0 / tempo;
+                    bpm = int(round(60000000.0 / tempo));
+                    print(f"BPM: {bpm}")
+                    print(f"cleaning set_tempo...")
+                    continue            
+            if msg.type == 'marker':
+                    print(f"METAMESSAGE marker time is: {msg} - found, cleaning...")
+                    continue
+
             if msg.type == 'note_on' and msg.velocity > 0:
+                if first_note_time is None:
+                   first_note_time = msg.time  # Speichere die Zeit der ersten Note
                 if not first_note_checked:
                     
-                    # Überprüfe, ob die Zeit der ersten Nachricht größer als 15000 ist
-                    if msg.time > 100:
-                        print(f"Erste Nachricht im Track {i} hat eine Zeit von {msg.time}. Setze auf 0.")
-                        msg.time = 0
+                    # Überprüfe, ob die Zeit der ersten Nachricht größer als 481 ist
+                    if msg.time > 481:
+                        print(f"Erste Nachricht im Track {i} hat eine Zeit von {msg.time}. ")
+                        msg.time = subtract_until_non_negative(msg.time, 1920)
+                        
                     first_note_checked = True
-                # notes_started = True
 
-            # if notes_started:
             new_track.append(msg)
 
         new_mid.tracks.append(new_track)
@@ -75,7 +99,7 @@ def process_directory(input_dir, output_dir):
                 remove_silence_from_midi(input_path, output_path)
 
 # Beispielaufruf
-input_directory = 'X:\\1_out'  # Pfad zu deinem Verzeichnis mit MIDI-Dateien
-output_directory = 'X:\\out' # Pfad zum Zielverzeichnis für bearbeitete Dateien
+input_directory = 'X:\\MIDIS'  # Pfad zu deinem Verzeichnis mit MIDI-Dateien
+output_directory = 'X:\\MIDIS_OUT' # Pfad zum Zielverzeichnis für bearbeitete Dateien
 
 process_directory(input_directory, output_directory)
